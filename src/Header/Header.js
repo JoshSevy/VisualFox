955d4d42b7eda3191f03ef9a81a2b7f91e74b37e
Joshua Sevy <joshuasevy@outlook.com>
2020-09-16 17:14:00 -0600

    Header jsx assertion made

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
-  return (<h1>Header</h1>)
+
+
+
+  return (
+    <article className="Header">
+      <Link>Logo</Link>
+      <h1 className="Header-title">Visualize Your Goals</h1>
+      <article className="Header-links">
+        <button>Home</button>
+        <button>Boards</button>
+        <button>New Board</button>
+      </article>
+    </article>
+  );
 }
+
+export default Header;
