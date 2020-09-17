40f548da95ba5a8b4c434c60ee3309dbbef80acf
Joshua Sevy <joshuasevy@outlook.com>
2020-09-17 12:48:57 -0600

    Add in Home component and associated files

---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => {
+
+
+  return(
+    <article className="Home">
+      <h1>Home</h1>
+    </article>
+  )
+}
+
+export default Home;
---
+++ src/Home/Home.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+describe('Home Component', () => {
+  it('should render Home Component correctly', () => {
+    
+  })
+})
