a063c2250a58365d5d436b57119f5d470a27634f
Joshua Sevy <joshuasevy@outlook.com>
2020-09-16 19:47:24 -0600

    Add base styling for the grid display

--- src/helpers/apiCall.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization:
-      'Client-ID e79s3GBFryMqW6-_prKVCwXfTSBC8hoyL3LSa4YphfI'
-  }
-});
