a8638bdfee1e2b57e0ce1631ed50a2b52859ad10
Joshua Sevy <joshuasevy@outlook.com>
2020-09-16 21:15:38 -0600

    Add in logo on header and adjusted styling for it

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './Header.scss'
+import  foxLogo  from '../assets/logo/foxLogo.png'
 
 
 const Header = () => {
@@ -10,7 +11,7 @@ const Header = () => {
 
   return (
     <article className="Header">
-      <Link to="/">Logo</Link>
+      <Link to="/"><img className="Header-logo" src={foxLogo}/></Link>
       <h1 className="Header-title">Visualize Your Goals</h1>
       <article className="Header-links">
         <button>Home</button>
