e52e922ecba845c0443574be0016999cca8d2328
Joshua Sevy <joshuasevy@outlook.com>
2020-09-16 18:41:36 -0600

    Add function to grab pictures from unsplash api

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 import Header from '../Header/Header'
@@ -7,6 +7,17 @@ import Header from '../Header/Header'
 
 
 const App = () => {
+  const [images, setImages] = useState();
+
+   onSearchSubmit = async (term) => {
+     const response = await unsplash.get(
+       "https://api.unsplash.com/search/photos",
+       {
+         params: { query: term },
+       }
+     );
+     setImages(response.data.results);
+   };
 
 
 
