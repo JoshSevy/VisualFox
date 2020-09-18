b993c19cb3e854bbf3f2996ec61e701dfc8acbdb
Joshua Sevy <joshuasevy@outlook.com>
2020-09-18 11:40:57 -0600

    Add universal button styling for project

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 import './Header.scss';
+import '../style/buttons.scss';
 import  foxLogo  from '../assets/logo/foxLogo.png';
 
 
 const Header = () => {
 
   return (
-    <article className="Header">
-      <Link to="/">
-        <img 
-          alt="Visual fox logo orange fox head"
-          className="Header-logo" 
-          src={foxLogo}/>
-      </Link>
-      <h1 className="Header-title">VisualFox</h1>
-      <article className="Header-links">
-        <button>Home</button>
-        <button>Boards</button>
-        <button>New Board</button>
+    <section>
+      <article className="Header">
+        <Link to="/">
+          <img
+            alt="Visual fox logo orange fox head"
+            className="Header-logo"
+            src={foxLogo}
+          />
+        </Link>
+        <h1 className="Header-title">VisualFox</h1>
       </article>
-    </article>
+      <article className="Header-links">
+        <NavLink to="/" className="btn btn-white">
+          <p className="btn-text">Home</p>
+        </NavLink>
+        <NavLink to="/boards" className="btn btn-white">
+          <p className="btn-text">Boards</p>
+        </NavLink>
+        <NavLink to="/prompt" className="btn btn-white">
+          <p className="btn-text">New Board</p>
+        </NavLink>
+      </article>
+    </section>
   );
 }
 
