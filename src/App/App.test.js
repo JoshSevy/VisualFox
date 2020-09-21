b3efaaace964d6aeccea4146e0cba29ea6fc9a5e
Joshua Sevy <joshuasevy@outlook.com>
2020-09-21 12:03:44 -0600

    Add full app run test in app.test.js

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -74,55 +74,55 @@ describe('App Component', () => {
     expect(categoryOptions).toHaveLength(7);
   })
 
-  // it('should check prompt changes and results async functions', async () => {
-  //   unsplashResponse.mockResolvedValueOnce({data : {results: testMockedFetchData}});
+  it('should check prompt changes and results async functions', async () => {
+    unsplashResponse.mockResolvedValueOnce({
+      data: { results: testMockedFetchData },
+    });
 
-  //   render(
-  //     <MemoryRouter>
-  //       <App />
-  //     </MemoryRouter>
-  //   );
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
     
-  //   const getStartedLink = screen.getByRole("link", {
-  //     name: /lets get started!/i,
-  //   });
+    const getStartedLink = screen.getByRole("link", {
+      name: /lets get started!/i,
+    });
 
-  //   fireEvent.click(getStartedLink);
+    fireEvent.click(getStartedLink);
 
-  //   const textInput = screen.getByPlaceholderText(/lets name this board/i);
-  //   const categorySelect = screen.getByRole("combobox", {
-  //     name: /select a category/i,
-  //   });
-  //   const subCategorySelect = screen.getByRole("combobox", {
-  //     name: /subcategory/i,
-  //   });
+    const textInput = screen.getByPlaceholderText(/lets name this board/i);
+    const categorySelect = screen.getByRole("combobox", {
+      name: /select a category/i,
+    });
+    const subCategorySelect = screen.getByRole("combobox", {
+      name: /subcategory/i,
+    });
     
-  //   fireEvent.change(textInput, {target : {value: "Test Board"}})
-  //   fireEvent.change(categorySelect, {target: {value: "fitness"}})
-  //   fireEvent.change(subCategorySelect, {target: {value: "lose weight"}})
+    fireEvent.change(textInput, {target : {value: "Test Board"}})
+    fireEvent.change(categorySelect, {target: {value: "fitness"}})
+    fireEvent.change(subCategorySelect, {target: {value: "lose weight"}})
 
-  //   const nextBtn = screen.getByRole('link', {name: /next/i})
+    const nextBtn = screen.getByRole('link', {name: /next/i})
     
-  //   expect(nextBtn).toBeInTheDocument();
+    expect(nextBtn).toBeInTheDocument();
 
-  //   fireEvent.click(nextBtn);
+    fireEvent.click(nextBtn);
 
-  //   const resultsTitle = await waitFor(() => screen.getByRole('heading', {name: /choose two/i}))
-  //   const continueBtn = await waitFor(() => screen.getByRole('link', {name: /continue/i}))
+    const resultsTitle = await waitFor(() => screen.getByRole('heading', {name: /choose two/i}))
+    const continueBtn = await waitFor(() => screen.getByRole('link', {name: /continue/i}))
 
-  //   expect(resultsTitle).toBeInTheDocument();
-  //   expect(continueBtn).toBeInTheDocument();
+    expect(resultsTitle).toBeInTheDocument();
+    expect(continueBtn).toBeInTheDocument();
 
-  //   fireEvent.click(continueBtn);
+    fireEvent.click(continueBtn);
 
-  //   const prompt2Title = screen.getByRole('heading', {name: /great work lets/i})
+    const prompt2Title = screen.getByRole('heading', {name: /great work lets/i})
 
-  //   expect(prompt2Title).toBeInTheDocument();
-  // })
+    expect(prompt2Title).toBeInTheDocument();
+  })
 
   it('should go route to error page if fetch request fails', () => {
-    unsplashResponse.mockRejectedValueOnce({});
-
     render(
       <MemoryRouter>
         <App />
@@ -153,6 +153,161 @@ describe('App Component', () => {
 
     fireEvent.click(nextBtn);
 
-    screen.debug()
+    const errorHeading = screen.getByRole('heading', {name: /sorry!/i});
+    expect(errorHeading).toBeInTheDocument();
+  })
+
+  it('should run through whole app process and return home', async() => {
+    unsplashResponse.mockResolvedValue({
+      data: { results: testMockedFetchData },
+    });
+
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const getStartedLink = screen.getByRole("link", {
+      name: /lets get started!/i,
+    });
+
+    fireEvent.click(getStartedLink);
+
+    const textInput = screen.getByPlaceholderText(/lets name this board/i);
+    const categorySelect = screen.getByRole("combobox", {
+      name: /select a category/i,
+    });
+    const subCategorySelect = screen.getByRole("combobox", {
+      name: /subcategory/i,
+    });
+
+    fireEvent.change(textInput, { target: { value: "Test Board" } });
+    fireEvent.change(categorySelect, { target: { value: "fitness" } });
+    fireEvent.change(subCategorySelect, { target: { value: "lose weight" } });
+
+    const nextBtn = screen.getByRole("link", { name: /next/i });
+
+    expect(nextBtn).toBeInTheDocument();
+
+    fireEvent.click(nextBtn);
+    
+    const continueBtn = await waitFor(() =>
+      screen.getByRole("link", { name: /continue/i })
+    );
+
+    fireEvent.click(continueBtn);
+
+    const prompt2Title = screen.getByRole("heading", {
+      name: /great work lets/i,
+    });
+
+    expect(prompt2Title).toBeInTheDocument();
+
+    const categorySelect2 = screen.getByRole("combobox", {
+      name: /select a category/i,
+    });
+    const subCategorySelect2 = screen.getByRole("combobox", {
+      name: /subcategory/i,
+    });
+
+    fireEvent.change(categorySelect2, { target: { value: "fitness" } });
+    fireEvent.change(subCategorySelect2, { target: { value: "lose weight" } });
+
+    const nextBtn2 = screen.getByRole("link", { name: /next/i });
+
+    expect(nextBtn2).toBeInTheDocument();
+
+    fireEvent.click(nextBtn2);
+
+    const continueBtn2 = await waitFor(() =>
+      screen.getByRole("link", { name: /continue/i })
+    );
+
+    expect(continueBtn2).toBeInTheDocument();
+
+    fireEvent.click(continueBtn2);
+
+    const prompt3Title = screen.getByRole("heading", {
+      name: /almost there keep it up/i,
+    });
+
+    expect(prompt3Title).toBeInTheDocument();
+
+    const categorySelect3 = screen.getByRole("combobox", {
+      name: /select a category/i,
+    });
+    const subCategorySelect3 = screen.getByRole("combobox", {
+      name: /subcategory/i,
+    });
+
+    fireEvent.change(categorySelect3, { target: { value: "fitness" } });
+    fireEvent.change(subCategorySelect3, { target: { value: "lose weight" } });
+
+    const nextBtn3 = screen.getByRole("link", { name: /next/i });
+
+    expect(nextBtn3).toBeInTheDocument();
+
+    fireEvent.click(nextBtn3);
+
+    const continueBtn3 = await waitFor(() =>
+      screen.getByRole("link", { name: /continue/i })
+    );
+
+    expect(continueBtn3).toBeInTheDocument();
+
+    fireEvent.click(continueBtn3);
+
+    const prompt4Title = screen.getByRole("heading", {
+      name: /last prompt home stretch/i,
+    });
+
+    expect(prompt4Title).toBeInTheDocument();
+
+    const categorySelect4 = screen.getByRole("combobox", {
+      name: /select a category/i,
+    });
+    const subCategorySelect4 = screen.getByRole("combobox", {
+      name: /subcategory/i,
+    });
+
+    fireEvent.change(categorySelect4, { target: { value: "fitness" } });
+    fireEvent.change(subCategorySelect4, { target: { value: "lose weight" } });
+
+    const nextBtn4 = screen.getByRole("link", { name: /next/i });
+
+    expect(nextBtn4).toBeInTheDocument();
+
+    fireEvent.click(nextBtn4);
+
+    const continueBtn4 = await waitFor(() =>
+      screen.getByRole("link", { name: /continue/i })
+    );
+
+    expect(continueBtn4).toBeInTheDocument();
+
+    fireEvent.click(continueBtn4);
+
+    const finalHeading = screen.getByRole('heading', {name: /congrats! you finished/i});
+    const buildBoardLink = screen.getByRole('link', {name: /build your board/i});
+
+    expect(finalHeading).toBeInTheDocument();
+    expect(buildBoardLink).toBeInTheDocument();
+
+    fireEvent.click(buildBoardLink);
+
+    const boardName = screen.getByRole('heading', {name: /test board/i});
+    const saveLink = screen.getByRole('link', {name: /save board/i});
+    const deleteLink = screen.getByRole('link', {name: /delete board/i});
+
+    expect(boardName).toBeInTheDocument();
+    expect(saveLink).toBeInTheDocument();
+    expect(deleteLink).toBeInTheDocument();
+
+    fireEvent.click(deleteLink);
+
+    const backHomeHeader = screen.getByRole('heading', {name: /visualize your goals/i})
+
+    expect(backHomeHeader).toBeInTheDocument();
   })
 })
